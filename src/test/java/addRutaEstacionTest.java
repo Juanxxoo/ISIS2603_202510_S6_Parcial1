4bdff2ad64265da8d9dd3a4e1432320ee683470a
Juanxxoo <jf.hortua@uniandes.edu.co>
2025-03-07 15:24:14 -0500

    avance

--- src/test/java/addRutaEstacionTest.java
+++ src/test/java/addRutaEstacionTest.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import co.edu.uniandes.dse.parcial1.entities.EstacionEntity;
+import co.edu.uniandes.dse.parcial1.entities.RutaEntity;
+import co.edu.uniandes.dse.parcial1.services.RutaEstacionService;
+import jakarta.transaction.Transactional;
+
+@DataJpaTest
+@Transactional
+@Import(RutaEstacionService.class)
 public class addRutaEstacionTest {
-    
+
+    @Autowired
+    RutaEstacionService rutaEstacionService;
+
+    @BeforeEach
+    void setUp() {
+        
+    }
+
 }
